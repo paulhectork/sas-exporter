97293e89b93f04b1832abdcdf128fe5b19e66639
paulhectork <paul.kervegan@hotmail.fr>
2026-03-14 00:27:39 +0100

    [ASYNC] switch from multiprocessing to pool

--- src/exporter.py
+++ src/exporter.py
@@ -1,8 +1,12 @@
+import asyncio
 from pathlib import Path
 from typing import List, Dict, Tuple
+from multiprocessing import Pool
 
 import requests
+import aiohttp
 from tqdm import tqdm
+from tqdm.asyncio import tqdm_asyncio
 
 from .utils import (
     SAS_ENDPOINT,
@@ -16,6 +20,8 @@ from .utils import (
 from .logger import logger
 
 
+# NOTE: max number of async processes to run simultaneously. otherwise, we can exceed what our OS can handle.
+# connector = aiohttp.TCPConnector(limit=32)
 
 def manifest_uri_to_short_id(manifest_uri: str) -> str:
     return manifest_uri.split("/")[-2]
@@ -27,13 +33,13 @@ def iiif_collection_to_manifest_uri_list(iiif_collection: Dict) -> List[str]:
         if m["@type"] == "sc:Manifest"
     ]
 
-def fetch_to_dict(url: str, raise_=True) -> Dict:
+async def fetch_to_dict(session:aiohttp.ClientSession, url: str, raise_=True) -> Dict:
     try:
-        r = requests.get(url)
-        if raise_:
-            r.raise_for_status()
-        # note: json_parse uses orjson, which is faster than native json => don't use requests.json()
-        return json_parse(r.text)
+        async with session.get(url) as response:
+            if raise_:
+                response.raise_for_status()
+            r_text = await response.text()
+            return json_parse(r_text)
     except Exception as e:
         if raise_:
             raise Exception
@@ -80,44 +86,62 @@ class SasExporter():
         return self
 
     def write_save_data(self) -> "SasExporter":
-        json_write(self.save_data, self.save_file)
+        json_write(self.save_data or {}, self.save_file)
         return self
 
-    # TODO error checking if request fails
-    def fetch_manifests(self) -> "SasExporter":
+    # TODO error handling if request fails
+    async def fetch_manifests(self) -> "SasExporter":
         manifests = []
-        collection = fetch_to_dict(self.endpoint_manifests)
-        manifests = iiif_collection_to_manifest_uri_list(collection)
+        async with aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(limit=32)
+        ) as session:
+            collection = await fetch_to_dict(session, self.endpoint_manifests)
+            manifests = iiif_collection_to_manifest_uri_list(collection)
         logger.info(f"Found {len(manifests)} for which to extract annotations.")
         self.manifests = manifests
         return self
 
-    # TODO multiprocessing
-    # TODO tqdm
-    # TODO error checking in request fails
-    # NOTE we're maybe gonna have a problem with writing to self.save_data: memory sharing between processes
-    # minimally, see: https://stackoverflow.com/a/29012047
-    def fetch_annotations(self) -> "SasExporter":
+    # TODO pagination
+    async def fetch_annotations(self, session: aiohttp.ClientSession, manifest_uri: str) -> None:
+        """
+        pipeline to download a single annotation_list
+        """
+        manifest_short_id = manifest_uri_to_short_id(manifest_uri)
+        search_api_endpoint = self.endpoint_annotations(manifest_short_id)
+        out_path = self.annotation_list_path(manifest_short_id)
+        data = await fetch_to_dict(session, search_api_endpoint)
+        self.write_annotation_list(data, out_path)
+        self.save_data[manifest_uri] = str(out_path)
+        # print(self.save_data)
+
+    # TODO error handling in request fails
+    # NOTE see if there's a problem with all fetch_annotations writing to self.save_data
+    async def fetch_annotations_all(self) -> "SasExporter":
         # self.save_data is a dict or { <manifest URI>: <path to downloaded annotationList> }
         manifests_to_download = [
             m for m in self.manifests if m not in self.save_data.keys()
         ]
-        for manifest_uri in manifests_to_download:
-            manifest_short_id = manifest_uri_to_short_id(manifest_uri)
-            search_api_endpoint = self.endpoint_annotations(manifest_short_id)
-            out_path = self.annotation_list_path(manifest_short_id)
-            data = fetch_to_dict(search_api_endpoint)
-            self.write_annotation_list(data, out_path)
-            self.save_data[manifest_uri] = out_path
+        tasks = []
+        async with aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(limit=32)
+        ) as session:
+            for m_uri in manifests_to_download:
+                tasks.append(self.fetch_annotations(session, m_uri))
+            await tqdm_asyncio.gather(
+                    *tasks,
+                    total=len(manifests_to_download),
+                    desc=f"DL annotation lists"
+                )
+        return self
 
+    async def pipeline_async(self) -> "SasExporter":
+        await self.fetch_manifests()
+        await self.fetch_annotations_all()
         return self
 
     def pipeline(self) -> "SasExporter":
         try:
-            (self
-                .fetch_manifests()
-                .fetch_annotations())
-
+            asyncio.run(self.pipeline_async())
         finally:
             self.write_save_data()
         return self
